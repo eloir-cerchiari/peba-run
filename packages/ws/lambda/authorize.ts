c47561e75fba94431f4f08592c0cc437a6c4cc49
Eloir Cerchiari <eloir.c@gmail.com>
2023-01-29 22:30:43 -0300

    load params from ssm

--- packages/ws/lambda/authorize.ts
+++ packages/ws/lambda/authorize.ts
@@ -1,65 +1,76 @@
 import axios from 'axios';
 import { Athlete } from '../src/model/athlete';
 import { AthleteRepository } from '../src/repo/athelete-repo';
-import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda"
-
+import {
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
 
 require('dotenv').config();
 
-export async function handle(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+export async function handle(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
   const body = JSON.parse(event.body || '{}');
 
-  if(!body?.code) return {
-    statusCode: 400,
-    body: JSON.stringify({
-      message: 'no code',
-    })
-  }
-  
-  try{
-    const tokenData = await authUser(body.code);
+  if (!body?.code)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'no code',
+      }),
+    };
 
+  try {
+    const tokenData = await authUser(body.code);
 
     const athleteRepo = new AthleteRepository();
     const athlete = await athleteRepo.get(tokenData.data.athlete.id);
     const response: Athlete[] = [];
-    if(!athlete || !athlete.id){
+
+    if (!athlete || !athlete.id) {
       const newAthlete = {
         id: tokenData.data.athlete.id,
-        name: tokenData.data.athlete.firstname + ' ' + tokenData.data.athlete.lastname,
+        name:
+          tokenData.data.athlete.firstname +
+          ' ' +
+          tokenData.data.athlete.lastname,
         strava: tokenData.data,
-      }
+      };
 
       response.push(await athleteRepo.insert(newAthlete));
     }
 
     return {
-      statusCode:201,
-      body: JSON.stringify(athlete?.strava?.athlete)
+      statusCode: 201,
+      body:  JSON.stringify({
+        att: athlete?.strava?.athlete,
+        created: athlete,
+      }),
     };
-  }catch(e){  
-    console.log(e.stack);
+  } catch (e) {
     return {
       statusCode: 500,
       body: JSON.stringify({
         message: 'error',
         stack: e.stack,
-      })
-    }
+      }),
+    };
   }
 }
 
 const authUser = async (code) => {
   const url = 'https://www.strava.com/api/v3/oauth/token';
+
   const data = {
     client_id: process.env.STRAVA_CLIENT_ID,
     client_secret: process.env.STRAVA_CLIENT_SECRET,
-    code: code ,
+    code: code,
     grant_type: 'authorization_code',
   };
 
   const result = await axios.post(url, data);
-  
 
   return result;
-}
+};
